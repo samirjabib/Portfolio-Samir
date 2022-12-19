a958cfe06a5e74338cdac090b8302518a83dc11a
samirjabib <samirdev9669@gmail.com>
2022-12-19 13:29:58 -0500

    fix img structure

--- src/Content.js
+++ src/Content.js
@@ -1,29 +1,28 @@
 // import images
-import Hero_person from "./assets/images/Hero/person.png";
+import Hero_person from "./assets/Hero/person.png";
 
-import figma from "./assets/images/Skills/figma.png";
-import sketch from "./assets/images/Skills/sketch.png";
-import ps from "./assets/images/Skills/ps.png";
-import reactjs from "./assets/images/Skills/react.png";
-import nodejs from "./assets/images/Skills/node.png";
-import python from "./assets/images/Skills/python.png";
+import figma from "./assets/Skills/figma.png";
+import sketch from "./assets/Skills/sketch.png";
+import ps from "./assets/Skills/ps.png";
+import reactjs from "./assets/Skills/react.png";
+import nodejs from "./assets/Skills/node.png";
+import python from "./assets/Skills/python.png";
 
-import services_logo1 from "./assets/images/Services/logo1.png";
-import services_logo2 from "./assets/images/Services/logo2.png";
-import services_logo3 from "./assets/images/Services/logo3.png";
+import services_logo1 from "./assets/Services/logo1.png";
+import services_logo2 from "./assets/Services/logo2.png";
+import services_logo3 from "./assets/Services/logo3.png";
 
-import project1 from "./assets/images/projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
-import person_project from "./assets/images/projects/person.png";
+import project1 from "./assets/projects/img1.png";
+import project2 from "./assets/projects/img2.png";
+import project3 from "./assets/projects/img3.png";
+import person_project from "./assets/projects/person.png";
 
-import avatar1 from "./assets/images/Testimonials/avatar1.png";
-import avatar2 from "./assets/images/Testimonials/avatar2.png";
-import avatar3 from "./assets/images/Testimonials/avatar3.png";
-import avatar4 from "./assets/images/Testimonials/avatar4.png";
+import avatar1 from "./assets/Testimonials/avatar1.png";
+import avatar2 from "./assets/Testimonials/avatar2.png";
+import avatar3 from "./assets/Testimonials/avatar3.png";
+import avatar4 from "./assets/Testimonials/avatar4.png";
 
-import Hireme_person from "./assets/images/Hireme/person.png";
-import Hireme_person2 from "./assets/images/Hireme/person2.png";
+import Hireme_person from "./assets/Hireme/person.png";
 
 // import icons from react-icons
 import { GrMail } from "react-icons/gr";
@@ -185,7 +184,6 @@ export const content = {
     title: "Hire Me",
     subtitle: "FOR YOUR PROJECTS",
     image1: Hireme_person,
-    image2: Hireme_person2,
     para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
     btnText: "Hire Me",
   },
--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
 import { HiMenuAlt2 } from "react-icons/hi";
+
+
+import { useState } from "react";
 import { createElement } from "react";
+
+//data dummie
+import { content } from "../Content";
 
 
 export const Navbar = () => {
+
     const [ showMenu, setShowMenu ] = useState(false);
     const [ active, setActive ] = useState(0);
 
